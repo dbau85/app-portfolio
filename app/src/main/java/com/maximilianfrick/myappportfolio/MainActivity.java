8f2cfc79ed086e297ea31bfadb2f73daff0f0edb
MaximilianFrick <maxi.frick@gmail.com>
2016-09-02 10:16:34 +0200

    created portfolio app

---
+++ app/src/main/java/com/maximilianfrick/myappportfolio/MainActivity.java
@@ -0,0 +1,32 @@
+package com.maximilianfrick.myappportfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @OnClick({R.id.btn_popular_movies, R.id.btn_build_it_bigger, R.id.btn_capstone,
+            R.id.btn_go_ubiqutous, R.id.btn_make_your_app_material, R.id.btn_stock_hawk})
+    public void onClickProject(Button button) {
+        showToast(button.getText().toString());
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, getString(R.string.toast_launch_app, text), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
